9bb289e81aa9648195fcd4ff5bd243c77092e4d4
Raghavendar-S <111366291+Raghavendar-S@users.noreply.github.com>
2023-10-09 23:35:03 +0530

    Update LoginPage.js

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -47,7 +47,7 @@ export function LoginPage() {
 
     if (Object.keys(errors).length === 0) {
       try{
-        const response = await axios.post('http://localhost:3001/register',{email,password})
+        const response = await axios.post('http://localhost:3001/login',{email,password})
         const token = response.data.token
         localStorage.setItem('token',token)
         navigate("/dashboard");
